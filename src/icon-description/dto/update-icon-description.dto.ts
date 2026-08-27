5d14b26817cddcf4de6e098d6b3b9986ad18a832
saMujbo <samuelbarrantes1@gmail.com>
2026-08-27 12:36:36 -0600

    DeleteMyWork

--- src/icon-description/dto/update-icon-description.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateIconDescriptionDto } from './create-icon-description.dto';
-
-export class UpdateIconDescriptionDto extends PartialType(CreateIconDescriptionDto) {}
--- src/icon-description/icon-description.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { IconDescriptionService } from './icon-description.service';
-import { IconDescriptionController } from './icon-description.controller';
-
-@Module({
-  controllers: [IconDescriptionController],
-  providers: [IconDescriptionService],
-})
-export class IconDescriptionModule {}
