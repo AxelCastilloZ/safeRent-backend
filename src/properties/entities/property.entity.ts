aac97c401688fb237ed9b8df89079ca3412b5625
saMujbo <samuelbarrantes1@gmail.com>
2026-08-26 21:46:57 -0600

    Entidad_Property

--- src/properties/entities/property.entity.ts
+++ src/properties/entities/property.entity.ts
@@ -1,1 +1,53 @@
-export class Property {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Property {
+
+    @PrimaryGeneratedColumn()
+    id!: number;
+
+    @Column({
+        type: 'varchar',
+        length: 100,
+    })
+    title!: string;
+
+    @Column({
+        type: 'text',
+    })
+    description!: string;
+
+    @Column({
+        type: 'decimal',
+        precision: 12,
+        scale: 2,
+    })
+    cost!: number;
+
+    @Column({
+        type: 'varchar',
+        length: 3,
+    })
+    typeOfCoin!: string;
+
+    @Column({
+        type: 'varchar',
+        length: 255,
+    })
+    address!: string;
+
+    @Column({
+        type: 'text',
+        array: true,
+        default: [],
+    })
+    images!: string[];
+
+    @Column({
+        type: 'boolean',
+        default: true,
+    })
+    isActive!: boolean;
+}
+
+
